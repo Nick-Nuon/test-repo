626d20a9a5c7a722775198f6d2e99e28f3c26690
Nick Nuon <myjpntrip@gmail.com>
2025-10-02 21:18:14 -0400

    save game

--- crypto/evp/enc_b64_avx2.h
+++ crypto/evp/enc_b64_avx2.h
@@ -6,9 +6,4 @@
 int encode_base64_avx2(EVP_ENCODE_CTX *ctx,
                        unsigned char *out, const unsigned char *src, int srclen,  int *steps_mod_lap);
 
-                       //TODO: temporary function only for benchmarking/testing purposes. Remove when all is said and done
-int encode_base64_avx2_alt(EVP_ENCODE_CTX *ctx,
-unsigned char *out, const unsigned char *src, int srclen, int disable_newlines, int *steps_mod_lap);
-
-
 #endif
