5345f67cd334fde2f719197f2e6565b1d655489c
Nick Nuon <myjpntrip@gmail.com>
2025-10-30 21:05:37 -0400

    fix i386 test attempt

--- test/test_base64_simdutf.c
+++ test/test_base64_simdutf.c
@@ -227,7 +227,7 @@ static int test_encode_line_lengths_reinforced(void)
 {
     const int trials = 50;
     #define MAX_INPUT_LEN 3000
-    unsigned int seed = 12345;
+    uint32_t seed = 12345;
     /* Generous output buffers (Update + Final + newlines), plus a guard byte */
     unsigned char out_simd[9000 * 2 + 1] = { 0 };
     unsigned char out_ref[9000 * 2 + 1] = { 0 };
